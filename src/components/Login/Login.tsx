d985cb3d2db5b40216be1439dbe3f152f3f92339
Jhorman Salazar <salazarjhorman181@gmail.com>
2025-05-27 11:48:43 -0500

    fixed login

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,31 +1,30 @@
 // TODO: Refactorizar con zustand, validar con zod y custom hook para la lógica
-// TODO: Mostrar mensaje de confirmación para cerrar sesión 
-// TODO: Spinner de carga al momento de redireccionar al iniciar sesión 
-// TODO: Responsive 
+// TODO: Mostrar mensaje de confirmación para cerrar sesión
+// TODO: Spinner de carga al momento de redireccionar al iniciar sesión
+// TODO: Responsive
 
-import { useState } from "react"
-import imagenLogo from "../../assets/Imagen-login.svg"
+import { useState } from "react";
+import imagenLogo from "../../assets/Imagen-login.svg";
 
 type LoginProps = {
-  handleSimulateAuth: (user: string, password: string) => void
-} 
+  handleSimulateAuth: (user: string, password: string) => void;
+};
 
-export default function Login({ handleSimulateAuth } : LoginProps) {
-
-  const [user, setUser] = useState("")
-  const [password, setPassword] = useState("")
+export default function Login({ handleSimulateAuth }: LoginProps) {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    handleSimulateAuth(user, password)
-  }
+    e.preventDefault();
+    handleSimulateAuth(user, password);
+  };
 
   return (
     <>
-      <form 
-          onSubmit={handleSubmit}
-          className=" grid grid-cols-2 items-center w-full text-sm h-screen "
-        >
+      <form
+        onSubmit={handleSubmit}
+        className=" grid grid-cols-2 items-center w-screen text-sm h-screen "
+      >
         <div className="flex flex-col justify-center mx-auto w-[55%] h-auto p-8 shadow-[0_2px_6px_rgba(0,0,0,0.142)] gap-y-1">
           <h1 className=" text-5xl font-medium">¡Bienvenido!</h1>
           <p className=" text-[rgb(137,137,137)] font-medium pt-2">
@@ -39,7 +38,7 @@ export default function Login({ handleSimulateAuth } : LoginProps) {
             type="text"
             id="user"
             value={user}
-            onChange={e => setUser(e.target.value)}
+            onChange={(e) => setUser(e.target.value)}
             className="p-3 mt-1.5 rounded-md border-2 border-gray-300"
           />
 
@@ -50,13 +49,13 @@ export default function Login({ handleSimulateAuth } : LoginProps) {
             type="password"
             id="password"
             value={password}
-            onChange={e => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
             className="p-3 mt-1.5 rounded-md border-2 border-gray-300"
           />
 
           <div className=" flex justify-between items-center pt-6">
             <div>
-              <input type="checkbox" id="checkbox"/>
+              <input type="checkbox" id="checkbox" />
               <label
                 htmlFor="checkbox"
                 className=" ml-2 cursor-pointer inline justify-center items-center"
@@ -83,7 +82,11 @@ export default function Login({ handleSimulateAuth } : LoginProps) {
         </div>
 
         <figure>
-          <img src={imagenLogo} alt="Imagen-bienvenida" className="flex justify-center items-center w-full h-screen bg-blue-800" />
+          <img
+            src={imagenLogo}
+            alt="Imagen-bienvenida"
+            className="flex justify-center items-center w-full h-screen bg-blue-800"
+          />
         </figure>
       </form>
     </>
