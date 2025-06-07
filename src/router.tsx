2cf3bf25799f904bcc7fc8720dc61146ebaa668f
Jose David <lujan9495@gmail.com>
2025-06-07 14:23:46 -0500

    Implement lazy loading for WelcomeSection with a fallback loading indicator in AppRouter

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { lazy, Suspense } from 'react'
 import Layout from './layouts/Layout'
-import WelcomeSection from './views/WelcomeSection'
+
+const WelcomeSection = lazy(() => import('./views/WelcomeSection'))
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route 
-            path="/" 
-            element={<WelcomeSection />} 
+          <Route
+            path="/"
+            element={
+              <Suspense
+                fallback={
+                  <div className="flex justify-center items-center h-screen font-medium text-gray-500 text-xl">
+                    Cargando...
+                  </div>
+                }
+              >
+                <WelcomeSection />
+              </Suspense>
+            }
             index
           />
         </Route>
